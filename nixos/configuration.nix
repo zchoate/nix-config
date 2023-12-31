44351bb9909d19bf7ff98d629b150203b2beabab
Zachary Choate <zach.a.choate@gmail.com>
2023-12-31 10:31:06 -0500

    add intial password hash

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -43,9 +43,8 @@
 
   users.users = {
     zach = {
-      # If you do, you can skip setting a root password by passing '--no-root-passwd' to nixos-install.
       # Be sure to change it (using passwd) after rebooting!
-      # initialPassword = "";
+      initialHashedPassword = "$7$CU..../....dBH.RrxoCFDZJgu7xlaAh.$mlMEd3JWE15bT9JB3SkauKmM2clKzUSSFw6HUwUz/42";
       isNormalUser = true;
       openssh.authorizedKeys.keys = [
         "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCudWw3jiJI90peXnoxQV3IDdmXpHdxNVUb6R1bUK42G3sXs89hAv7h3v6cH0GSgFLqRflqwvqiFgXk0nGKAVAxE+zKS06kLgx0/Lidcf7VmQxXrJhEO5X5cLMyvUey7AuvBtSRgJrJy6BMJ8vp5oCctlme3tjxwK80SQ+5ZBJbG0v8eB6E3szsu6rVNGsIcOxUKLAU/HryZbZRMeT8us71xeccmecMEpnM93rS9bv4muf70uuhl2cxs3LllOIx9l1bpRp29NPilACenlKFpFiAvSs8vyJLbmm+uT8bLE93YPLh+iQPXgrSSOiMcdZ421mw5yrpoj0hPGGMUKFw5JXg5G5XzhpS2NcncGd8Zxknw+BNYb/KulWaItnPYG/bBmru8He8jCo8sxaYSrXCrOPLqVOFBwyp5Tl4qcqliAuXulcyDXH2NhLCNnLYlH0IQvxoOzJZZVDO8gawLXoE0I+OfAY5ZFdR4flbVmhTEQ5BJHdvYILxXlz/lbQ5c3697Ik="
